052ddf419c549670eaa56a500dcdf0da1ea4d1aa
louislvq <louis.leveque@outlook.com>
2022-05-12 01:44:40 +0200

    Utils : ft_exit added

--- include/utils.h
+++ include/utils.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: arudy <arudy@student.42.fr>                +#+  +:+       +#+        */
+/*   By: lleveque <lleveque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 11:28:53 by lleveque          #+#    #+#             */
-/*   Updated: 2022/05/11 15:27:51 by arudy            ###   ########.fr       */
+/*   Updated: 2022/05/12 00:30:57 by lleveque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,9 @@
 
 void	free_all(t_data *data);
 void	ft_free(void *ptr, t_data *data);
+void	ft_exit(t_data *data, int exit_value);
+void	ft_exit_rgb_error(char c, char *msg, t_data *data, int value);
+void	ft_exit_texture_error(char *id, char *msg, t_data *data, int value);
 void	*ft_malloc(size_t size, t_data *data);
 char	*get_next_line(int fd, t_data *data);
 t_data	*init_data(char *input);
