e19a6c45b0184f76af7238a14ff4454e114088b7
Arthur <arudy@student.42.fr>
2022-05-11 11:02:28 +0200

    .h

--- include/get_next_line.h
+++ include/get_next_line.h
@@ -1,25 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   cub3d.h                                            :+:      :+:    :+:   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: arudy <arudy@student.42.fr>                +#+  +:+       +#+        */
+/*   By: lleveque <lleveque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/11 10:09:54 by lleveque          #+#    #+#             */
-/*   Updated: 2022/05/11 11:00:41 by arudy            ###   ########.fr       */
+/*   Created: 2021/12/09 12:11:30 by lleveque          #+#    #+#             */
+/*   Updated: 2021/12/13 14:37:03 by lleveque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CUB3D_H
-# define CUB3D_H
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-# include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
 
-# include "parsing.h"
-# include "../mlx/mlx.h"
-# include "get_next_line.h"
-# include "../mlx/mlx_int.h"
+char	*get_next_line(int fd);
+char	*str_to_out(char *str);
+char	*next_str(char *str);
+char	*read_line(int fd, char *str);
+size_t	ft_strlen(char *s);
+int		ft_strchr(char *s, char c);
+char	*ft_strjoin(char *s1, char *s2);
 
 #endif
--- include/parsing.h
+++ include/parsing.h
@@ -1,25 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   cub3d.h                                            :+:      :+:    :+:   */
+/*   parsing.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: arudy <arudy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/11 10:09:54 by lleveque          #+#    #+#             */
-/*   Updated: 2022/05/11 11:00:41 by arudy            ###   ########.fr       */
+/*   Created: 2022/05/11 10:57:41 by arudy             #+#    #+#             */
+/*   Updated: 2022/05/11 11:01:52 by arudy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CUB3D_H
-# define CUB3D_H
-
-# include <stdio.h>
-# include <stdlib.h>
-# include <unistd.h>
-
-# include "parsing.h"
-# include "../mlx/mlx.h"
-# include "get_next_line.h"
-# include "../mlx/mlx_int.h"
+#ifndef PARSING_H
+# define PARSING_H
 
 #endif
