21445957163e2cd033d3062ea242f512434fd420
louislvq <louis.leveque@outlook.com>
2022-05-12 01:43:36 +0200

    Utils : ft_exit added

---
+++ srcs/utils/ft_exit.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_exit.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lleveque <lleveque@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/11 23:13:23 by lleveque          #+#    #+#             */
+/*   Updated: 2022/05/12 00:30:42 by lleveque         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/cub3d.h"
+
+void	ft_exit_texture_error(char *id, char *msg, t_data *data, int value)
+{
+	ft_putstr_fd("Error\nIdentifier ", 2);
+	ft_putstr_fd(id, 2);
+	ft_putstr_fd(msg, 2);
+	ft_putstr_fd("\n", 2);
+	free_all(data);
+	exit(value);
+}
+
+void	ft_exit_rgb_error(char c, char *msg, t_data *data, int value)
+{
+	ft_putstr_fd("Error\n", 2);
+	write(2, &c, 1);
+	ft_putstr_fd(msg, 2);
+	ft_putstr_fd("\n", 2);
+	free_all(data);
+	exit(value);
+}
+
+void	ft_exit(t_data *data, int exit_value)
+{
+	free_all(data);
+	exit(exit_value);
+}
