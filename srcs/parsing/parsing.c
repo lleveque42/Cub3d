1b67ecff2e090968d0eaa2b565bccd6c31655324
lleveque42 <louis.leveque@outlook.com>
2022-05-12 15:24:19 +0200

    Parsing : check_walls added

--- srcs/parsing/parsing.c
+++ srcs/parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: lleveque <lleveque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 10:28:30 by arudy             #+#    #+#             */
-/*   Updated: 2022/05/12 12:03:20 by lleveque         ###   ########.fr       */
+/*   Updated: 2022/05/12 15:20:22 by lleveque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,14 +26,14 @@ void	scan_buff(t_data *data, char *buff)
 	{
 		while (*buff && ft_is_whitespace(*buff))
 			buff++;
-		if (*buff && *buff != '\n')
+		if (*buff && *buff != '\n' && !check_map_line(buff, data))
 			data->map_fd->height++;
 		else
 			data->map_fd->begin++;
 	}
 	else if (!check_line(buff) && !data->map_fd->ended)
 		data->map_fd->ended = 1;
-	else if ((check_line(buff) && data->map_fd->ended) || check_map_line(buff))
+	else if ((check_line(buff) && data->map_fd->ended) || check_map_line(buff, data))
 		ft_exit_message("Map is invalid", data, 1);
 	else if (!data->map_fd->ended)
 		data->map_fd->height++;
@@ -82,6 +82,32 @@ void	fill_map(t_data *data)
 	data->map[j] = NULL;
 }
 
+void	scan_map(char **s, t_data *data)
+{
+	int	x;
+	int	y;
+
+	y = -1;
+	while (s[++y])
+	{
+		x = -1;
+		if (y == 0 || y == data->map_fd->height - 1)
+		{
+			if (check_wall_only(s[y]))
+				ft_exit_message("Map is not valid: not surrounded by \
+walls", data, 1);
+			continue ;
+		}
+		while (s[y][++x])
+		{
+			if (s[y][x] != ' ')
+				if (check_wall(s, x, y, data))
+					ft_exit_message("Map is not valid: not surrounded by \
+walls", data, 1);
+		}
+	}
+}
+
 int	parsing(t_data *data)
 {
 	fill_data(data);
@@ -93,5 +119,6 @@ int	parsing(t_data *data)
 	}
 	data->map = ft_malloc(sizeof(char *) * (data->map_fd->height + 1), data);
 	fill_map(data);
+	scan_map(data->map, data);
 	return (0);
 }
--- srcs/parsing/utils.c
+++ srcs/parsing/utils.c
@@ -6,11 +6,54 @@
 /*   By: lleveque <lleveque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 21:58:41 by lleveque          #+#    #+#             */
-/*   Updated: 2022/05/12 11:57:10 by lleveque         ###   ########.fr       */
+/*   Updated: 2022/05/12 15:19:33 by lleveque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/cub3d.h"
+
+int	check_wall_only(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] && s[i] != '\n')
+	{
+		if (!(s[i] == ' ' || s[i] == '1'))
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	check_wall(char **s, int x, int y, t_data *data)
+{
+	if (y != 0 && s[y - 1][x])
+	{
+		if (s[y - 1][x] != ' ' && s[y - 1][x] != '1')
+			return (1);
+		if (s[y - 1][x + 1] && s[y - 1][x + 1] != ' ' && s[y - 1][x + 1] != '1')
+			return (1);
+	}
+	if (y != data->map_fd->height - 1 && s[y + 1][x])
+	{
+		if (s[y + 1][x] != ' ' && s[y + 1][x] != '1')
+			return (1);
+		if (s[y + 1][x + 1] && s[y + 1][x + 1] != ' ' && s[y + 1][x + 1] != '1')
+			return (1);
+	}
+	if (y != 0 && s[y - 1][x] && x > 0)
+		if (s[y - 1][x - 1] && s[y - 1][x - 1] != ' ' && s[y - 1][x - 1] != '1')
+			return (1);
+	if (x > 0 && y != data->map_fd->height)
+		if (s[y + 1][x - 1] && s[y + 1][x - 1] != ' ' && s[y + 1][x - 1] != '1')
+			return (1);
+	if (x > 0 && s[y][x - 1] && s[y][x - 1] != ' ' && s[y][x - 1] != '1')
+		return (1);
+	if (s[y][x] && s[y][x + 1] && s[y][x + 1] != ' ' && s[y][x + 1] != '1')
+		return (1);
+	return (0);
+}
 
 int	check_line(char *buff)
 {
@@ -26,72 +69,24 @@ int	check_line(char *buff)
 	return (0);
 }
 
-int	check_map_line(char *buff)
+int	check_map_line(char *buff, t_data *data)
 {
 	int	i;
 
 	i = 0;
 	while (buff[i] && buff[i] != '\n')
 	{
-		while (buff[i] && ft_is_whitespace(buff[i]))
+		while (buff[i] && buff[i] == ' ')
 			i++;
 		if (buff[i] != '1')
-			return (1);
+			ft_exit_message("Map is invalid", data, 1);
 		while (buff[i] && (buff[i] == '1' || buff[i] == '0' || buff[i] == 'N'
 				|| buff[i] == 'E' || buff[i] == 'S' || buff[i] == 'W'))
 			i++;
 		if (buff[i - 1] && buff[i - 1] != '1')
-			return (1);
+			ft_exit_message("Map is invalid", data, 1);
 	}
 	if (buff[i] && buff[i] != '\n')
-		return (1);
+		ft_exit_message("Map is invalid", data, 1);
 	return (0);
-}
-
-int	check_rgb_value(char **rgb, char c, t_data *data)
-{
-	int	i;
-	int	len;
-
-	i = 0;
-	while (rgb[i])
-	{
-		len = 0;
-		while (rgb[i][len] && ft_isdigit(rgb[i][len]))
-			len++;
-		if (len > 3 || (len == 3 && ft_atoi(rgb[i]) > 255))
-			ft_exit_rgb_error(c, " is invalid in file, RGB colors in range \
-[0, 255]", data, i);
-		i++;
-	}
-	return (0);
-}
-
-int	check_rgb(char **rgb, char c, t_data *data)
-{
-	int	i;
-	int	j;
-
-	i = -1;
-	while (rgb[++i])
-	{
-		j = 0;
-		while (i < 2 && rgb[i][j])
-		{
-			if (!ft_isdigit(rgb[i][j]))
-				return (1);
-			j++;
-		}
-		while (i == 2 && rgb[i][j] && ft_isdigit(rgb[i][j]))
-			j++;
-		while (i == 2 && rgb[i][j] && rgb[i][j] != '\n')
-		{
-			if (!ft_is_whitespace(rgb[i][j]))
-				return (1);
-			j++;
-		}
-		if (i > 2 || (i == 2 && !ft_isdigit(rgb[i][0])))
-			return (1);
-	}
-	return (check_rgb_value(rgb, c, data));
 }
