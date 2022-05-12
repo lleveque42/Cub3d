d7fa708402d264fa96f1d830f9c69e5d3b061815
Arthur <arudy@student.42.fr>
2022-05-12 11:02:09 +0200

    Parsing : 2nd gnl ok

--- srcs/parsing/parsing.c
+++ srcs/parsing/parsing.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lleveque <lleveque@student.42.fr>          +#+  +:+       +#+        */
+/*   By: arudy <arudy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 10:28:30 by arudy             #+#    #+#             */
-/*   Updated: 2022/05/12 02:06:07 by lleveque         ###   ########.fr       */
+/*   Updated: 2022/05/12 10:59:09 by arudy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,8 +38,6 @@ void	scan_buff(t_data *data, char *buff)
 	}
 	else
 		data->map_fd->height++;
-	printf("data.begin %d\n", data->map_fd->begin);
-	printf("data.height %d\n", data->map_fd->height);
 }
 
 void	fill_data(t_data *data)
@@ -48,34 +46,37 @@ void	fill_data(t_data *data)
 
 	while (1)
 	{
-		buff = get_next_line(data->in_fd, 0, data);
+		buff = get_next_line(data->in_fd, data);
 		if (!buff)
 			break ;
 		scan_buff(data, buff);
 		ft_free(buff, data);
 	}
+	close(data->in_fd);
 }
 
 void	fill_map(t_data *data)
 {
 	char	*buff;
 	int		i;
+	int		j;
 
-	i = 1;
-	buff = get_next_line(data->in_fd, data);
-	printf("%s\n", buff);
-	printf("map begin = %d\n", data->map_fd->begin);
-	while (i < data->map_fd->begin)
+	i = -1;
+	j = 0;
+	data->in_fd = open(data->filename, O_RDONLY);
+	// Checker open ret
+	while (++i < data->map_fd->begin)
 	{
 		buff = get_next_line(data->in_fd, data);
-		printf("fwefwefwe\n");
 		if (!buff)
 			break ;
-		i++;
 		ft_free(buff, data);
 	}
+	while (j < data->map_fd->height)
+	{
+		data->map[j++] = get_next_line(data->in_fd, data);
+	}
 	printf("%s\n", buff);
-	printf("begin = %d\n", i);
 }
 
 int	parsing(t_data *data)
@@ -87,6 +88,7 @@ int	parsing(t_data *data)
 		ft_putstr_fd(data->filename, 2);
 		ft_putstr_fd(" file not valid\n", 2);
 	}
+	data->map = ft_malloc(sizeof(char *) * (data->map_fd->height + 1), data);
 	fill_map(data);
 	return (0);
 }
