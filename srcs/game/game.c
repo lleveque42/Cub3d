7c8bc425ef9ecb3bab68bf8376d5d6a4bb4652d3
louislvq <louis.leveque@outlook.com>
2022-05-24 13:26:31 +0200

    Game : 2 windows, minimap && 3d

--- srcs/game/game.c
+++ srcs/game/game.c
@@ -6,7 +6,7 @@
 /*   By: lleveque <lleveque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 14:02:26 by arudy             #+#    #+#             */
-/*   Updated: 2022/05/24 12:59:40 by lleveque         ###   ########.fr       */
+/*   Updated: 2022/05/24 13:26:03 by lleveque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -171,8 +171,8 @@ void	render_ray(t_data *data)
 			data->ray->pwd = data->ray->sdy - data->ray->ddy;
 
 		// Calculate height of the line to draw & and coor of pixels to fill
-		printf("win_height = %d\n", data->win_height);
-		printf("pwd = %f\n", data->ray->pwd);
+		// printf("win_height = %d\n", data->win_height);
+		// printf("pwd = %f\n", data->ray->pwd);
 		data->ray->line_h = (int)(data->win_height / data->ray->pwd);
 		data->ray->draw_start = -data->ray->line_h / 2 + data->win_height / 2;
 		if (data->ray->draw_start < 0)
@@ -183,8 +183,7 @@ void	render_ray(t_data *data)
 		// choose wall color
 		color = RED;
 		if (data->ray->side == 0)
-			color = color / 2;
-
+			color = BLUE;
 		// // draw ray on screen
 		// int	i = 0;
 		// while (i < data->win_width)
@@ -193,11 +192,11 @@ void	render_ray(t_data *data)
 			while (y < data->win_height)
 			{
 				if (y >= data->ray->draw_start && y <= data->ray->draw_end)
-					pixel_put(data, x, y, color);
+					pixel_put2(data, x, y, color);
 				else if (y > data->ray->draw_start)
-					pixel_put(data, x, y, WHITE);
+					pixel_put2(data, x, y, WHITE);
 				else
-					pixel_put(data, x, y, BLACK);
+					pixel_put2(data, x, y, BLACK);
 				++y;
 			}
 			// ++i;
@@ -212,9 +211,10 @@ int	render_image(t_data *data)
 	get_pos(data);
 	if (data->player->change)
 	{
-		// render_player(data);
+		render_player(data);
 		render_ray(data);
 		mlx_put_image_to_window(data->mlx->ptr, data->mlx->win, data->mlx->img, 0, 0);
+		mlx_put_image_to_window(data->mlx->ptr2, data->mlx->win2, data->mlx->img2, 0, 0);
 	}
 	data->player->change = 0;
 	return (0);
@@ -255,36 +255,31 @@ void	game(t_data *data)
 	data->win_height = data->map_fd->height * TILE_SIZE;
 	data->win_width = data->map_fd->width * TILE_SIZE;
 	data->mlx->ptr = mlx_init();
-	// data->mlx->ptr2 = mlx_init();
+	data->mlx->ptr2 = mlx_init();
 	if (!data->mlx->ptr)
 		ft_exit(data, "Can't init mlx ptr");
 	data->mlx->win = mlx_new_window(data->mlx->ptr, data->win_width, data->win_height, "QubtroiD");
-	// data->mlx->win2 = mlx_new_window(data->mlx->ptr2, data->win_width, data->win_height, "QubtroiD");
+	data->mlx->win2 = mlx_new_window(data->mlx->ptr2, data->win_width, data->win_height, "QubtroiD");
 	if (!data->mlx->win)
 		ft_exit(data, "Can't init mlx window");
 	data->mlx->img = mlx_new_image(data->mlx->ptr, data->map_fd->width * TILE_SIZE, data->map_fd->height * TILE_SIZE);
-	// data->mlx->img2 = mlx_new_image(data->mlx->ptr2, data->map_fd->width * TILE_SIZE, data->map_fd->height * TILE_SIZE);
+	data->mlx->img2 = mlx_new_image(data->mlx->ptr2, data->map_fd->width * TILE_SIZE, data->map_fd->height * TILE_SIZE);
 	if (!data->mlx->img)
 		ft_exit(data, "Can't init mlx image");
 	data->mlx->addr = mlx_get_data_addr(data->mlx->img, &data->mlx->bpp,
 					&data->mlx->line_length, &data->mlx->endian);
-	// data->mlx->addr2 = mlx_get_data_addr(data->mlx->img2, &data->mlx->bpp,
-	// 				&data->mlx->line_length, &data->mlx->endian);
+	data->mlx->addr2 = mlx_get_data_addr(data->mlx->img2, &data->mlx->bpp,
+					&data->mlx->line_length, &data->mlx->endian);
 	if (!data->mlx->addr)
 		ft_exit(data, "Can't init mlx addr");
 	render_minimap(data);
 	render_player(data);
 	render_ray(data);
 	mlx_put_image_to_window(data->mlx->ptr, data->mlx->win, data->mlx->img, 0, 0);
-	// mlx_put_image_to_window(data->mlx->ptr2, data->mlx->win2, data->mlx->img2, 0, 0);
+	mlx_put_image_to_window(data->mlx->ptr2, data->mlx->win2, data->mlx->img2, 0, 0);
 	mlx_hook(data->mlx->win, 2, 1, &key_event, data);
-	// mlx_hook(data->mlx->win2, 2, 1, &key_event, data);
 	mlx_loop_hook(data->mlx->ptr, &render_image, data);
-	// mlx_loop_hook(data->mlx->ptr2, &render_image2, data);
 	mlx_hook(data->mlx->win, 17, 17, &ft_exit_esc, data);
-	// mlx_hook(data->mlx->win2, 17, 17, &ft_exit_esc, data);
 	mlx_hook(data->mlx->win, 3, 10, &key_release, data);
-	// mlx_hook(data->mlx->win2, 3, 10, &key_release, data);
 	mlx_loop(data->mlx->ptr);
-	// mlx_loop(data->mlx->ptr2);
 }
