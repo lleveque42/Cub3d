33f7c9efa61732d989af77f41a33ccde6048513b
louislvq <louis.leveque@outlook.com>
2022-05-24 13:05:28 +0200

    utils : pixel_put2 added for window2

--- srcs/utils/mlx_utils.c
+++ srcs/utils/mlx_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   mlx_utils.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: arudy <arudy@student.42.fr>                +#+  +:+       +#+        */
+/*   By: lleveque <lleveque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/20 11:22:44 by arudy             #+#    #+#             */
-/*   Updated: 2022/05/24 11:20:43 by arudy            ###   ########.fr       */
+/*   Updated: 2022/05/24 12:38:34 by lleveque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,15 @@ void	pixel_put(t_data *data, int x, int y, int color)
 	char	*dst;
 
 	dst = data->mlx->addr + (y * data->mlx->line_length)
+		+ (x * data->mlx->bpp / 8);
+	*(unsigned int *)dst = color;
+}
+
+void	pixel_put2(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->mlx->addr2 + (y * data->mlx->line_length)
 		+ (x * data->mlx->bpp / 8);
 	*(unsigned int *)dst = color;
 }
