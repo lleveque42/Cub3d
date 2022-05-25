d01752023e5a40aae1d2607caef861b1a8051a12
louislvq <louis.leveque@outlook.com>
2022-05-25 11:58:05 +0200

    Init : init with good values

--- srcs/parsing/utils.c
+++ srcs/parsing/utils.c
@@ -6,7 +6,7 @@
 /*   By: lleveque <lleveque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 21:58:41 by lleveque          #+#    #+#             */
-/*   Updated: 2022/05/24 18:21:56 by lleveque         ###   ########.fr       */
+/*   Updated: 2022/05/25 11:29:07 by lleveque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,31 @@ void	init_plane(t_data *data)
 {
 	if (data->player->dir == 'N')
 	{
-		data->ray->plane_x = 0;
-		data->ray->plane_y = 0.66;
+		data->player->dir_y = -1;
+		data->player->dir_x = 0;
+		data->ray->plane_x = 0.66;
+		data->ray->plane_y = 0;
 	}
 	else if (data->player->dir == 'S')
 	{
-		data->ray->plane_x = 0;
-		data->ray->plane_y = -0.66;
-	}
-	else if (data->player->dir == 'E')
-	{
+		data->player->dir_y = 1;
+		data->player->dir_x = 0;
 		data->ray->plane_x = -0.66;
 		data->ray->plane_y = 0;
 	}
+	else if (data->player->dir == 'E')
+	{
+		data->player->dir_y = 0;
+		data->player->dir_x = 1;
+		data->ray->plane_x = 0;
+		data->ray->plane_y = 0.66;
+	}
 	else if (data->player->dir == 'W')
 	{
-		data->ray->plane_x = 0.66;
-		data->ray->plane_y = 0;
+		data->player->dir_y = 0;
+		data->player->dir_x = -1;
+		data->ray->plane_x = 0;
+		data->ray->plane_y = -0.66;
 	}
 }
 
@@ -45,19 +53,9 @@ void	find_player_data(char **s, int x, int y, t_data *data)
 	data->player->y = y + 0.5;
 	data->player->old_y = data->player->y;
 	data->player->old_x = data->player->x;
-	if (data->player->dir == 'N')
-		data->player->angle = 3 * PI / 2;
-	else if (data->player->dir == 'S')
-		data->player->angle = PI / 2;
-	else if (data->player->dir == 'E')
-		data->player->angle = 0;
-	else if (data->player->dir == 'W')
-		data->player->angle = PI;
-	data->player->dir_x = cos(data->player->angle) * 5;
-	data->player->dir_y = sin(data->player->angle) * 5;
+	init_plane(data);
 	data->player->old_dx = data->player->dir_x;
 	data->player->old_dy = data->player->dir_y;
-	init_plane(data);
 	data->player->change = 1;
 }
 
