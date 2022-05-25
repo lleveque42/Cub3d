b8fe40c0939cb7e9cfbf9e3719bf2ee620281ec6
louislvq <louis.leveque@outlook.com>
2022-05-25 11:57:41 +0200

    .h : speeeeeeeeeeed

--- include/cub3d.h
+++ include/cub3d.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: arudy <arudy@student.42.fr>                +#+  +:+       +#+        */
+/*   By: lleveque <lleveque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 10:09:54 by lleveque          #+#    #+#             */
-/*   Updated: 2022/05/24 18:44:46 by arudy            ###   ########.fr       */
+/*   Updated: 2022/05/25 11:53:53 by lleveque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,8 +33,12 @@
 # define SCREEN_W			1200
 # define SCREEN_H			900
 # define PI					3.1415926535
-# define SPEED_TEST			0.02
-# define ROT_SPEED			0.03
+// # define SPEED_TEST			0.015 // LOUIS
+// # define ROT_SPEED			0.015 // LOUIS
+# define SPEED_TEST			0.1 // LOUIS VALGRIND
+# define ROT_SPEED			0.1 // LOUIS VALGRIND
+// # define SPEED_TEST			0.02
+// # define ROT_SPEED			0.01
 # define RED				0x00FF7F7F
 # define REDDD				0x99605c
 # define WHITE				0x00FFFFFF
