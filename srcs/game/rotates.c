3faafb592bf665358d79a70c618fb1a63aa43784
louislvq <louis.leveque@outlook.com>
2022-05-25 11:58:26 +0200

    Game : rotate player + cam all OK

--- srcs/game/rotates.c
+++ srcs/game/rotates.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   rotates.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: arudy <arudy@student.42.fr>                +#+  +:+       +#+        */
+/*   By: lleveque <lleveque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/16 10:06:08 by lleveque          #+#    #+#             */
-/*   Updated: 2022/05/25 10:28:53 by arudy            ###   ########.fr       */
+/*   Updated: 2022/05/25 11:16:19 by lleveque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	rotate_right(t_data *data)
 {
-	// double	old_plane_x;
+	double	old_plane_x;
 
 	old_plane_x = data->ray->plane_x;
 	(void)old_plane_x;
@@ -22,16 +22,14 @@ void	rotate_right(t_data *data)
 	data->player->old_dy = data->player->dir_y;
 	data->player->dir_x = data->player->dir_x * cos(ROT_SPEED) - data->player->dir_y * sin(ROT_SPEED);
 	data->player->dir_y = data->player->old_dx * sin(ROT_SPEED) + data->player->dir_y * cos(ROT_SPEED);
-	// data->ray->plane_x = data->ray->plane_x * cos(ROT_SPEED) - data->ray->plane_y * sin(ROT_SPEED);
-	// data->ray->plane_y = old_plane_x * sin(ROT_SPEED) + data->ray->plane_y * cos(ROT_SPEED);
+	data->ray->plane_x = data->ray->plane_x * cos(ROT_SPEED) - data->ray->plane_y * sin(ROT_SPEED);
+	data->ray->plane_y = old_plane_x * sin(ROT_SPEED) + data->ray->plane_y * cos(ROT_SPEED);
 	data->player->change = 1;
-	printf("right plane_x = %f\n", data->ray->plane_x);
-	printf("right plane_y = %f\n", data->ray->plane_y);
 }
 
 void	rotate_left(t_data *data)
 {
-	// double	old_plane_x;
+	double	old_plane_x;
 
 	old_plane_x = data->ray->plane_x;
 	(void)old_plane_x;
@@ -39,9 +37,7 @@ void	rotate_left(t_data *data)
 	data->player->old_dy = data->player->dir_y;
 	data->player->dir_x = data->player->dir_x * cos(-ROT_SPEED) - data->player->dir_y * sin(-ROT_SPEED);
 	data->player->dir_y = data->player->old_dx * sin(-ROT_SPEED) + data->player->dir_y * cos(-ROT_SPEED);
-	// data->ray->plane_x = data->ray->plane_x * cos(-ROT_SPEED) - data->ray->plane_y * sin(-ROT_SPEED);
-	// data->ray->plane_y = old_plane_x * sin(-ROT_SPEED) + data->ray->plane_y * cos(-ROT_SPEED);
+	data->ray->plane_x = data->ray->plane_x * cos(-ROT_SPEED) - data->ray->plane_y * sin(-ROT_SPEED);
+	data->ray->plane_y = old_plane_x * sin(-ROT_SPEED) + data->ray->plane_y * cos(-ROT_SPEED);
 	data->player->change = 1;
-	printf("left plane_x = %f\n", data->ray->plane_x);
-	printf("left plane_y = %f\n", data->ray->plane_y);
 }
